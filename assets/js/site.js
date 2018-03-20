ad6ec025eef42248478a301bfe649eed47e6623c
Jerin Lee <taurenjerin@qq.com>
2018-03-20 13:08:42 +0800

    增加百度统计的代码。

--- assets/js/site.js
+++ assets/js/site.js
@@ -20,3 +20,12 @@ $(document)
         // insert footer
         $("<div/>").load("/footer.html").insertAfter($("#footerline"))
     });
+
+// 百度统计
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?15391ff66eb3bee047797a0260c8d331";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();
